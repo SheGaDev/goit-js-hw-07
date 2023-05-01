eec9350ee5fd1d0273ef7333aa632715da2d2a5e
Vadym Prokhorenko <prokhorenko.vadim.98@gmail.com>
2023-05-01 23:28:33 +0300

    new commit

---
+++ js/02-lightbox.js
@@ -0,0 +1,34 @@
+import { galleryItems } from './gallery-items.js';
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("beforeend", galleryItems
+    .map(({ preview, original, description }) => `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`).join("\n"));
+// let instance;
+// const onClick = (e) => {
+//     e.preventDefault();
+//     if (e.target.classList.contains("gallery")) return;
+//     const source = e.target.dataset.source;
+//     instance = basicLightbox.create(`<img src="${source}">`);
+//     instance.show();
+// };
+// const onEscape = (e) => {
+//     if (e.code !== "Escape"
+//         || !instance) return;
+//     instance.close();
+//     instance = null;
+// }
+// galleryEl.addEventListener("click", onClick);
+// galleryEl.addEventListener("keydown", onEscape);
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionType: "alt",
+  });
